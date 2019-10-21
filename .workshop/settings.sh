40f6d7597af4add2c9d1c6e542622306782c67d0
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2019-10-21 12:41:19 +1100

    Move kustomize config to sub directory.

--- .workshop/settings.sh
+++ .workshop/settings.sh
@@ -1,5 +1,7 @@
 WORKSHOP_NAME=lab-asciidoc-sample
 WORKSHOP_IMAGE=quay.io/openshifthomeroom/lab-asciidoc-sample:1.8
+WORKSHOP_TITLE="AsciiDoc Sample"
+WORKSHOP_DESCRIPTION="A sample workshop using AsciiDoc"
 RESOURCE_BUDGET=medium
 MAX_SESSION_AGE=3600
 IDLE_TIMEOUT=300
